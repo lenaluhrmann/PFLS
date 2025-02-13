e72fdc1c94a15e6fe9888683b2c73b1ef05bc4bb
Lena Luhrmann <lena-luhrmann@web.de>
2025-02-13 14:14:11 +0100

    second version

--- EXC-004/generate-combined-data.sh
+++ EXC-004/generate-combined-data.sh
@@ -8,6 +8,22 @@ do
     cp "${directory}checkm.txt" "../COMBINED-DATA/${name}-CHECKM.txt"
     cp "${directory}gtdb.gtdbtk.tax" "../COMBINED-DATA/${name}-GTDB-TAX.txt"
     cp "${directory}bins/bin-unbinned.fasta" "../COMBINED-DATA/${name}_UNBINNED.fa"
+	completness=$(awk 'NR>=3 {print $13}' "${directory}checkm.txt" | tr -s '\n' ' ')
+    contamination=$(awk 'NR>=3 {print $14}' "${directory}checkm.txt" | tr '\n' ' ')
+
+	for value in $completness; do
+    if (( $(echo "$value > 50.00" | bc -l) )); then
+        echo "$value ist ein MAG"
+    fi
+done
+	for value in $contamination; do
+	    if (( $(echo "$value < 5.00" | bc -l) )); then
+       	 echo "$value ist ein bin"
+    fi
+done
+done 
+exit
+
     for fasta in ${directory}bins/*;
         do 
             file_name="${fasta#${directory}bins/}"
